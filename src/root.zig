496058e2dbf00145c2cd9f3444f9e3e0b2f5fd7b
andy5995 <arch_stanton5995@protonmail.com>
2024-03-11 23:18:10 -0500

    Complete fixing errors, add more tests
    Thanks to dimdin for the test suggestions
    https://ziggit.dev/t/zig-build-completes-but-zig-build-test-shows-errors/3537/5?u=andy5995

--- src/root.zig
+++ src/root.zig
@@ -20,8 +20,9 @@ const Deck = struct {
         var deck: Deck = .{ .cards = undefined, .top = 0 };
         var i: usize = 0;
         // inline for (std.meta.fields(Suit)) |suit| {
-        for (0..3) |suit| {
+        for (0..4) |suit| {
             for (0..13) |value| {
+                // std.debug.print("{d}\n", .{value});
                 deck.cards[i] = Card{ .suit = @enumFromInt(suit), .value = @as(u4, @intCast(value)) + 1 };
                 i += 1;
             }
@@ -29,20 +30,15 @@ const Deck = struct {
         return deck;
     }
 
-    pub fn shuffle(deck: *Deck) void {
-        // This should be moved out of the library; no need
-        // to seed every time a deck is shuffled. How to pass rng from
-        // from another function?
-        var rng = std.rand.DefaultPrng.init(1);
+    pub fn shuffle(deck: *Deck, rng: *const std.rand.Random) void {
         for (deck.cards, 0..) |_, item| {
-            const i = deck.cards[item];
-            const j = rng.random().int(usize) % deck.cards.len;
+            const i = item;
+            const j = rng.int(usize) % deck.cards.len;
             const temp = deck.cards[i];
             deck.cards[i] = deck.cards[j];
             deck.cards[j] = temp;
         }
     }
-
 
     pub fn getTopCard(deck: *Deck) ?Card {
         if (deck.top >= deck.cards.len) return null;
@@ -54,7 +50,22 @@ const Deck = struct {
 
 test "Deck operations" {
     var deck = Deck.init();
-    Deck.shuffle(&deck);
+    try std.testing.expectEqual(Suit.Clubs, deck.cards[0].suit);
+    try std.testing.expectEqual(1, deck.cards[0].value);
+    try std.testing.expectEqual(Suit.Clubs, deck.cards[1].suit);
+    try std.testing.expectEqual(2, deck.cards[1].value);
+    try std.testing.expectEqual(Suit.Diamonds, deck.cards[13].suit);
+    try std.testing.expectEqual(1, deck.cards[13].value);
+    try std.testing.expectEqual(Suit.Hearts, deck.cards[26].suit);
+    try std.testing.expectEqual(1, deck.cards[26].value);
+    try std.testing.expectEqual(Suit.Spades, deck.cards[48].suit);
+    try std.testing.expectEqual(13, deck.cards[51].value);
+
+    // var rng = std.rand.DefaultPrng.init(@as(u64, @intCast(std.time.milliTimestamp())));
+    var rng = std.rand.DefaultPrng.init(1);
+    Deck.shuffle(&deck, &rng.random());
     const card = Deck.getTopCard(&deck) orelse return;
+    try std.testing.expectEqual(Suit.Clubs, card.suit);
+    try std.testing.expectEqual(12, card.value);
     std.debug.print("Top card: Suit = {}, Value = {}\n", .{ card.suit, card.value });
 }
