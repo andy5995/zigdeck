7e66792243613821d4d64d8b8c770ff559de471c
andy5995 <arch_stanton5995@protonmail.com>
2024-03-12 23:20:11 -0500

    Add faces

--- src/root.zig
+++ src/root.zig
@@ -7,9 +7,25 @@ pub const Suit = enum {
     Spades,
 };
 
+pub const Face = enum(u8) {
+    Ace = 1,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+};
+
 pub const Card = struct {
     suit: Suit,
-    value: u4,
+    face: Face,
 };
 
 pub const Deck = struct {
@@ -19,11 +35,9 @@ pub const Deck = struct {
     pub fn init() Deck {
         var deck: Deck = .{ .cards = undefined, .top = 0 };
         var i: usize = 0;
-        // inline for (std.meta.fields(Suit)) |suit| {
-        for (0..4) |suit| {
-            for (0..13) |value| {
-                // std.debug.print("{d}\n", .{value});
-                deck.cards[i] = Card{ .suit = @enumFromInt(suit), .value = @as(u4, @intCast(value)) + 1 };
+        inline for (std.meta.fields(Suit), 0..) |_, suit| {
+            inline for (std.meta.fields(Face), 1..) |_, face| {
+                deck.cards[i] = Card{ .suit = @enumFromInt(suit), .face = @enumFromInt(face) };
                 i += 1;
             }
         }
@@ -51,21 +65,21 @@ pub const Deck = struct {
 test "Deck operations" {
     var deck = Deck.init();
     try std.testing.expectEqual(Suit.Clubs, deck.cards[0].suit);
-    try std.testing.expectEqual(1, deck.cards[0].value);
+    try std.testing.expectEqual(Face.Ace, deck.cards[0].face);
     try std.testing.expectEqual(Suit.Clubs, deck.cards[1].suit);
-    try std.testing.expectEqual(2, deck.cards[1].value);
+    try std.testing.expectEqual(Face.Two, deck.cards[1].face);
     try std.testing.expectEqual(Suit.Diamonds, deck.cards[13].suit);
-    try std.testing.expectEqual(1, deck.cards[13].value);
+    try std.testing.expectEqual(Face.Ace, deck.cards[13].face);
     try std.testing.expectEqual(Suit.Hearts, deck.cards[26].suit);
-    try std.testing.expectEqual(1, deck.cards[26].value);
+    try std.testing.expectEqual(Face.Ace, deck.cards[26].face);
     try std.testing.expectEqual(Suit.Spades, deck.cards[48].suit);
-    try std.testing.expectEqual(13, deck.cards[51].value);
+    try std.testing.expectEqual(Face.King, deck.cards[51].face);
 
     // var rng = std.rand.DefaultPrng.init(@as(u64, @intCast(std.time.milliTimestamp())));
     var rng = std.rand.DefaultPrng.init(1);
     Deck.shuffle(&deck, &rng.random());
     const card = Deck.getTopCard(&deck) orelse return;
     try std.testing.expectEqual(Suit.Clubs, card.suit);
-    try std.testing.expectEqual(12, card.value);
-    std.debug.print("Top card: Suit = {}, Value = {}\n", .{ card.suit, card.value });
+    try std.testing.expectEqual(Face.Queen, card.face);
+    std.debug.print("Top card: Suit = {}, Value = {}\n", .{ card.suit, card.face });
 }
